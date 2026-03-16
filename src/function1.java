39c0944dbb1c3bbad1367b2fd82ffbc2c6c75609
Amit Kumar <ak6697292@gmail.com>
2026-03-16 11:17:28 +0530

    no message

---
+++ src/function1.java
@@ -0,0 +1,10 @@
+public class function1 {
+    static int add(int a, int b){
+        return a+b;
+    }
+    public static void main (String[] args){
+        int result = add(6,9);
+        System.out.println("Sum =" +result);
+    }
+
+}
