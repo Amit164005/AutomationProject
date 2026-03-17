8d4dae026dcbe4c0348de802f8c6a237bf78ac72
Amit Kumar <ak6697292@gmail.com>
2026-03-17 10:15:36 +0530

    Commit

---
+++ src/GreatestNumber.java
@@ -0,0 +1,12 @@
+public class GreatestNumber {
+    public static void main (String[] args)
+    {
+        int num1 = 45, num2 = 625;
+        if (num1 == num2)
+            System.out.println("both are equal");
+        else if (num1 > num2)
+            System.out.println(num1 + " num1 is greater");
+        else
+            System.out.println(num2 + " num2 is greater");
+    }
+}
