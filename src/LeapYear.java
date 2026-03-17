df9323435cef5f158b39cfcc04d800a54b57009d
Amit Kumar <ak6697292@gmail.com>
2026-03-17 17:26:10 +0530

    Commit

---
+++ src/LeapYear.java
@@ -0,0 +1,17 @@
+public class LeapYear {
+    public static void main (String[] args)
+    {
+        int year = 2026;
+
+        if (year % 400 == 0)
+            System.out.println(year + " is a Leap year");
+
+        else if (year % 4 == 0 && year % 100 != 0)
+            System.out.println(year + " is a Leap year");
+
+        else
+            System.out.println(year + "is not Leap year");
+
+        }
+    }
+
