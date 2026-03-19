4201704e8b104a1ab594334d79785cfe0f758443
Amit Kumar <ak6697292@gmail.com>
2026-03-19 19:11:56 +0530

    no message

---
+++ src/CheckLeapYear.java
@@ -0,0 +1,9 @@
+public class CheckLeapYear {
+    public static void main (String[] args)
+    {
+        int year = 2020;
+
+        if (year % 400 == 0)
+            System.out.println(year + " is a Leap Year");
+    }
+}
